622908516eb5f428c174714787c4d8b1c2161a91
thattemperature <2719023332@qq.com>
2026-05-12 00:45:46 +0800

    Update "thattem-mode-line".

--- pkgs/applications/editors/emacs/elisp-packages/manual-packages/thattem-mode-line/package.nix
+++ pkgs/applications/editors/emacs/elisp-packages/manual-packages/thattem-mode-line/package.nix
@@ -13,13 +13,13 @@
 melpaBuild {
 
   pname = "thattem-mode-line";
-  version = "0-unstable-2026-05-11";
+  version = "0-unstable-2026-05-12";
 
   src = fetchFromGitHub {
     owner = "thattemperature";
     repo = "thattem-mode-line";
-    rev = "31c655902730bbdbfcfb2740b38dd9b46ded6a60";
-    hash = "sha256-bpMhMypXRGJFi9iQk3hsyw+vzfytG91xXPbPSKPLzMI=";
+    rev = "0cfd4517ff3eb8b1bfe065d34886454f553f6bac";
+    hash = "sha256-AEb7Xh7F6CwbPEogCEvN9nWlONKvXPvjT0X5lnT4pvM=";
   };
 
   packageRequires = [
