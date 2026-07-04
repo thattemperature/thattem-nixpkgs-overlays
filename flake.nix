375e297b874fcce19976e8f39bdade4f045a3b0a
thattemperature <2719023332@qq.com>
2026-07-05 00:18:15 +0800

    Downgrade mihomo for clash-verge-rev.

--- flake.nix
+++ flake.nix
@@ -11,9 +11,10 @@
         default = import ./pkgs/top-level (
           with self.overlays;
           [
-            gnome-extensions
-            emacs-packages
             by-name
+            emacs-packages
+            gnome-extensions
+            workarounds
           ]
         );
 
@@ -22,6 +23,32 @@
         emacs-packages = import ./pkgs/top-level/emacs-packages-overlay.nix ./pkgs/applications/editors/emacs/elisp-packages/manual-packages.nix;
 
         gnome-extensions = import ./pkgs/top-level/gnome-extensions-overlay.nix ./pkgs/desktops/gnome/extensions/manuallyPackaged.nix;
+
+        workarounds =
+          # Workaround: downgrade mihomo for clash-verge-rev
+          (
+            final: prev: {
+
+              mihomo_1_19_26 = prev.mihomo.overrideAttrs (old: rec {
+
+                version = "1.19.26";
+
+                src = final.fetchFromGitHub {
+                  owner = "MetaCubeX";
+                  repo = "mihomo";
+                  rev = "v${version}";
+                  hash = "sha256-As0MqIGHs1Gn+aUWpeFsC231n9v7lBNmGlQdAwVWcJs=";
+                };
+
+                vendorHash = "sha256-ySpBMR/djPPs1aTw7yiCrCFxDFsvRfTJEChg8v1C408=";
+              });
+
+              clash-verge-rev = prev.clash-verge-rev.override {
+
+                mihomo = final.mihomo_1_19_26;
+              };
+            }
+          );
 
       };
     };
